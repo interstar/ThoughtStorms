80f045c365b59f38a24bf0606f504325aa287f42
phil jones <interstar@gmail.com>
2012-03-14 04:09:32 +0000

    init

---
+++ README.rst
@@ -0,0 +1,37 @@
+Project ThoughtStorms
+=====================
+
+Project ThoughtStorms is an umbrella to pull together and refresh a number of things I've worked on over the last 10 or so years. It's focussed on Ward Cunningham's Smallest Federated Wiki ( https://github.com/WardCunningham/Smallest-Federated-Wiki ) which looks increasingly exciting as the solution to problems that I've had; some of which I didn't even recognise until I saw SFW.
+
+My Motivation
+-------------
+
+* I want to update and refresh my personal wiki, ThoughtStorms, which currently resides at http://www.nooranch.com/synaesmedia/wiki/wiki.cgi .
+
+* I want to have a wiki with some of the features I prototyped in SdiDesk, a personal wiki-like notebook I wrote for Windows back in the mid-noughties.
+
+Why Use Smallest Federated Wiki
+-------------------------------
+
+* SFW separates into minimal back-end server and a rich client running in the browser. That means I can host personal wikis on my local machine and public copies in the cloud, while the client can run on desktop and mobile devices.
+
+* The client and one implementation of the server are written in CoffeeScript. A language I'm using and liking a lot.
+
+* SFW supports different content-types which can have their own rendering and editing interfaces. And you can extend it with plugins that add new types. That's something I need in order to reproduce the things I did in SdiDesk and some further projects which have never been released.
+
+* The drag-and-drop refactoring looks like it will be very useful to clean-up and reorganise ThoughtStorms.
+
+* The emphasis on making "federation" easy solves dilemmas like : I'd like to keep some personal information privately in my local wiki but also use a public wiki as a publishing medium. Oh, and I'd like to access it in the cloud, from anywhere. In a sense it seems that SFW is bringing git-like distributed thinking to wiki. I can have a local 
+
+
+ 
+
+
+
+
+
+Tasks
+-----
+
+* Import 
+
---
+++ scripts/importFiles.py
@@ -0,0 +1,76 @@
+import random
+import re
+import json
+import sys
+
+import pdb
+
+class SfwPage :
+    def __init__(self,title) :
+        if '/' in title : title=(title.split('/'))[-1]
+        self.title = title
+        self.story = []
+        self.journal = []
+
+    def randomId(self) :
+        return "%08d%08d" % (random.randint(0,10**8),random.randint(0,10**8))
+        
+    def slug(self,s) :
+        re.sub(s,'\s','-')
+        re.sub(s,'\/','--')
+        re.sub(s,'[^A-Za-z0-9-]','')
+        s = s.lower()
+        
+
+    def para(self,data,type="paragraph") :
+        s = data.encode("ascii","xmlcharrefreplace")
+        return {"type":type, "id":self.randomId(), "text":s}
+        
+    def addPara(self,data,type="paragraph") :
+        self.story.append(self.para(data,type))
+        
+        
+    def page(self) :
+        return {"title":self.title, "story":self.story, "journal":self.journal}
+    
+    
+    def __str__(self) :
+        return json.dumps(self.page(),ensure_ascii=False)
+    
+    
+class UseMod2SFW :
+    def __init__(self, fName) :
+
+        self.page = SfwPage(fName)
+        
+        with open(fName) as f :
+            s = f.read()
+            s = s.decode("Latin1","replace")
+            lines = re.split('((\r)*\n)',s)
+
+            build = u""
+            for l in lines :
+                if l :
+                    build = build + l
+                else :
+                    self.page.addPara(self.camelCaseLinks(build),"wikish")
+                    build = ""
+            self.page.addPara(self.camelCaseLinks(build))
+
+    def camelCaseLinks(self,s) :
+        return re.sub(u'([A-Z][a-z]+([A-Z][a-z]+)+)','[[\\1]]',s)
+
+    def __str__(self) :              
+        return self.page.__str__()
+            
+if __name__ == '__main__' :
+    for v in sys.argv[1:] :
+        try :
+            conv = UseMod2SFW(v)
+            x = u"%s" % conv
+            f = open('output/%s'%(conv.page.title.lower()),'w')
+            f.write(x)
+            f.close()
+        except Exception, e:
+            print "error %s in %s" % (e, v)
+            # pdb.post_mortem()
