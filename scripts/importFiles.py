635d62714c7671219d8fffc64185f4c79f060eec
phil jones <interstar@gmail.com>
2012-03-16 00:42:09 +0000

    fixed links, breaking lists from non-lists

--- scripts/importFiles.py
+++ scripts/importFiles.py
@@ -37,7 +37,30 @@ class SfwPage :
     def __str__(self) :
         return json.dumps(self.page(),ensure_ascii=False)
     
-    
+
+class Build :
+  def __init__(self,s="") : self.build = s
+  def __str__(self) : return self.build
+  def __call__(self,s="") :
+    self.build = self.build+s
+    return self.build
+
+
+def splitter(criteria,page) :
+    # splits page by criteria, yields each piece
+    i = 0
+    l = ""        
+    while (i < len(page)) :
+        if criteria(page,i) :
+            yield l
+            l = page[i]
+        else :
+            l = l + page[i]
+        i=i+1
+    yield l
+
+
+
 class UseMod2SFW :
     def __init__(self, fName) :
 
@@ -46,20 +69,51 @@ class UseMod2SFW :
         with open(fName) as f :
             s = f.read()
             s = s.decode("Latin1","replace")
-            lines = re.split('((\r)*\n)',s)
+            # remove \r
+            s = s.replace('\r','\n')
+            
+            #lines = s.split('\n')
+
+            def outlineSplitterFactory() :
+                osFlag = [False]
+                def g(chunk,i) :
+                    if i == len(chunk)-1 : return False
+                    if (chunk[i]=='\n') :
+                        if (osFlag[0]) :
+                            if (chunk[i+1]=='*') :
+                                osFlag[0] = False       
+                                return True
+                        else :
+                            if (chunk[i+1]!='*') :
+                                osFlag[0] = True
+                                return True
+                    return False
+                return g
 
             build = u""
-            for l in lines :
-                if l :
-                    build = build + l
-                else :
-                    self.page.addPara(self.camelCaseLinks(build),"wikish")
-                    build = ""
-            self.page.addPara(self.camelCaseLinks(build))
+            try :
+                for block in (splitter(outlineSplitterFactory(),s)) :
+                    self.page.addPara(self.process(block),"wikish")
+            except Exception, e:
+                print "error %s in %s" % (e, v)
+                pdb.post_mortem()
 
+                
+
+    def process(self,s) :
+        return self.camelCaseLinks(self.externalLinks(s))
+                
     def camelCaseLinks(self,s) :
-        return re.sub(u'([A-Z][a-z]+([A-Z][a-z]+)+)','[[\\1]]',s)
+        return re.sub(u'(\s)([A-Z][a-z]+([A-Z][a-z]*)+)','\\1[[\\2]]',s)
 
+    def externalLinks(self,s) :
+        s = re.sub(u'(http://[\S]+\.[\S]+.[\S]+[\S]*)','[\\1 \\1]',s)
+        s = re.sub(u'(https://[\S]+\.[\S]+.[\S]+[\S]*)','[\\1 \\1]',s)
+        s = re.sub(u'(ftp://[\S]+\.[\S]+.[\S]+[\S]*)','[\\1 \\1]',s)
+        s = re.sub(u'(ftps://[\S]+\.[\S]+.[\S]+[\S]*)','[\\1 \\1]',s)
+        return s
+        
+    
     def __str__(self) :              
         return self.page.__str__()
             
