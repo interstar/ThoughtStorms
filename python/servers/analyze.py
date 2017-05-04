72dffd57138aac48873b252f53ff52f46367957c
Phil Jones <interstar@gmail.com>
2017-05-04 11:59:04 -0300

    first analysis ... extract youtube block from a youtube url

--- python/servers/analyze.py
+++ python/servers/analyze.py
@@ -1,4 +1,6 @@
 
+import re
+	
 class Analyzer :
 	def make_form(self) :
 		return """
@@ -9,12 +11,35 @@ class Analyzer :
 	</div>
 </form>""" 
 
+	
+	def analyze_line(self,l) :
+		return self.youtube(l)
+		
+	def youtube(self,l) :
+		print "in youtube"
+		print l
+		e = re.compile(r"https://www.youtube.com/watch\?v=(\S+)")
+		m = e.match(l)
+		print m
+		if m :
+			print "aa ", m
+			print "bb ", m.groups()
+			print "cc ", m.group(1)
+			return """
+[<YOUTUBE
+id : %s
+>] """ % m.group(1)
+		else : 
+			return l
+
 		
 	
 	def analyze(self,data) : 
+		xs = [self.analyze_line(l) for l in data.split("\n")]
+		
 		return """
 		<textarea name="data" cols="120" rows="30">Data Was : 
 		
 %s		
-		</textarea>""" % data
+		</textarea>""" % "\n".join(xs)
 
