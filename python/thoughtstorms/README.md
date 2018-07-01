2c2c218cd7f1853ca1cc0d1d7670383ab3522dda
Phil Jones <interstar@gmail.com>
2018-07-01 17:38:30 -0300

    moving thoughtstorms components to a package

---
+++ python/thoughtstorms/README.md
@@ -0,0 +1,14 @@
+
+## ThoughtStorms Library
+
+**Components for use in Project ThoughtStorms, ThoughtStorms Wiki and others**
+
+This project breaks a number of components used in ThoughtStorms Wiki and associated software from "Project ThoughtStorms" into a re-usable library that can be included via pip.
+
+
+### Components :
+
+* PageStore
+* TSWiki
+
+
---
+++ python/thoughtstorms/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='thoughtstorms',
+      version='0.0.1',
+      description='Components from Project ThoughtStorms (ThoughtStorms wiki and associated software)',
+      long_description="""Project ThoughtStorms is the software behind ThoughtStorms Wiki and some associated other programs.
+      
+We're now breaking these components out into a separate library that is available on PyPI and can be installed with pip.
+      
+      """,
+      long_description_content_type='text/markdown',
+      url='https://github.com/interstar/ThoughtStorms',
+      author='Phil Jones',
+      author_email='interstar@gmail.com',
+      license='MIT',
+      packages=['thoughtstorms'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers'],
+      zip_safe=False)
