102e91d1b45af6d42f909091389bd3f0cd401481
interstar <interstar@gmail.com>
2019-04-01 01:54:48 -0300

    moving stuff around

---
+++ tools/conversion/wikish2md.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2
+
+from sys import stdin, stdout, argv
+
+from thoughtstorms.legacy import Wikish2Markdown
+
+chef = Wikish2Markdown()
+
+
+for fName in argv[1:] :
+	print fName
+	oName = fName.split("/")[-1]
+	print oName
+
+	with open(fName) as f:
+		with open(oName,"w") as out :
+			for l in f.readlines() :
+				out.write(chef.line(l)+"\n")
+
